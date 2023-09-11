3c33758c5135cea7ede61dbe87bf67c78f670320
Daniel <danielius@estotyvilnius.com>
2023-09-11 17:57:46 +0300

    Added Graph behaviour

---
+++ Runtime/Scripts/Graph/GraphBehaviour.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Framework
+{
+	public abstract class GraphBehaviour : ScriptableObject
+	{
+		public abstract List<GraphBehaviour> GetReferencedNodes();
+
+		#if UNITY_EDITOR
+
+		public Vector2 nodePosition;
+
+		#endif
+	}
+}
