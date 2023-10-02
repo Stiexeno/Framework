ff763ee85c047eaae9b481de570eb4185458cc04
Daniel <danka9536@gmail.com>
2023-10-02 21:00:37 +0300

    Modified GlobalResourceSettings

--- Runtime/Core/ResourcesGlobalSetting.cs
+++ Runtime/Core/ResourcesGlobalSetting.cs
@@ -1,48 +1,33 @@
-﻿#if UNITY_EDITOR
-using UnityEditor;
-#endif
-
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Framework.Core
 {
 	public class ResourcesGlobalSetting<T> : ScriptableObject where T : ScriptableObject
 	{
-		private const string GLOBAL_SETTING_PATH = "GlobalSettings/{0}";
-			
 		private static T active;
-		
+
 		protected static T Active
 		{
 			get
 			{
 				if (active == null)
 				{
-					var targetPath = string.Format(GLOBAL_SETTING_PATH, typeof(T).Name);
-					var globalSetting = Resources.Load<T>(targetPath);
+					var globalSetting = Resources.Load<T>(typeof(T).Name);
 
 					if (globalSetting == null)
 					{
+						Debug.LogWarning($"Failed to load {typeof(T).Name}");
 						var instance = CreateInstance<T>();
-						#if UNITY_EDITOR
-						try
-						{
-							AssetDatabase.CreateAsset(instance, $"Assets/Resources/{targetPath}.asset");
-							AssetDatabase.Refresh();
-						} catch (System.Exception exception)
-						{
-							Debug.LogError(exception);
-						}
-						#endif
 
 						active = instance;
-					} else
+					}
+					else
 					{
 						active = globalSetting;
 					}
 				}
-				
-				return active; 
+
+				return active;
 			}
 		}
 	}
